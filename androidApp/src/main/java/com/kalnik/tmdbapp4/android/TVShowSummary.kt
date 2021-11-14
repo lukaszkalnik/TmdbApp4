ac423996033bb644b9010fea272315a74c2cc5b8
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2021-11-14 21:20:29 +0100

    Use LazyColumn for TVShowSummaryList

--- androidApp/src/main/java/com/kalnik/tmdbapp4/android/TVShowSummary.kt
+++ androidApp/src/main/java/com/kalnik/tmdbapp4/android/TVShowSummary.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
@@ -54,11 +56,10 @@ fun TVShowSummaryList(
     modifier: Modifier,
     tvShows: List<TVShow>
 ) {
-    Column(
-        modifier = modifier then
-                Modifier.verticalScroll(state = rememberScrollState())
+    LazyColumn(
+        modifier = modifier
     ) {
-        tvShows.map {
+        items(tvShows) {
             TVShowSummary(
                 name = it.name,
                 overview = it.overview,
