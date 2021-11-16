2ed924cda525d9cf78f7bcb94ee55d430c120e22
bogdanov <iceposhta@gmail.com>
2021-11-16 12:32:09 +0100

    reformat code

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,22 +1,19 @@
-import SwiftUI
 import shared
+import SwiftUI
 
 @main
 struct iOSApp: App {
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    var body: some Scene {
+        WindowGroup {
+            TVShowSummaryView()
+        }
+    }
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         startKoin()
         return true
     }
